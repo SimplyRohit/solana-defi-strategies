d08efb1fadeb0407afe896233281d6f0f649e8f1
SimplyRohit <simplyrohit.tech@gmail.com>
2025-11-17 14:58:18 +0530

    chore : changes

---
+++ apps/web/src/components/wallet/privy.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId="your-privy-app-id"
+      clientId="your-app-client-id"
+      config={{
+        embeddedWallets: {
+          solana: {
+            createOnLogin: "users-without-wallets",
+          },
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
--- packages/rpc/client/provider.tsx
+++ packages/rpc/client/provider.tsx
@@ -1,11 +1,10 @@
-import {
-  QueryClient,
-  QueryClientProvider as RQProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 
 export const queryClient = new QueryClient();
 
 export function ReactQueryProvider({ children }: { children: ReactNode }) {
-  return <RQProvider client={queryClient}>{children}</RQProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
